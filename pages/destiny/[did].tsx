99278b6e88b15d261f0b5ca225deba55c3c51f84
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-10-05 13:33:04 +0800

    get one destiny after post useefevt

--- pages/destiny/[did].tsx
+++ pages/destiny/[did].tsx
@@ -1,9 +1,68 @@
+import { NextPage, GetServerSideProps } from "next";
+import ErrorPage from "next/error";
 import { useRouter } from "next/router";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
-const Destiny: FC = () => {
+interface Props {
+  destiny?: {
+    id: string;
+    name: string;
+    description: string;
+    rating: string;
+    image: string;
+    category: string;
+  };
+}
+
+// getServersideprops to get destiny data from server
+export const getServersideprops: GetServerSideProps = async (context) => {
+  console.log("dasd");
+
+  const { did } = context.query;
+  console.log(did);
+  const res = await fetch(`http://localhost:4000/v1/destiny/${did}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  console.log(res, "sadsa");
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      destiny: data.destiny,
+    },
+  };
+};
+
+const Destiny = (props: Props) => {
+  console.log(props.destiny, "sad");
   const router = useRouter();
   const { did } = router.query;
+
+  //   if (!router.isFallback) {
+  //    return <ErrorPage statusCode={404} />
+  //  }
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/destiny/${did}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data.destiny));
+  });
+
   return <div>{did}</div>;
 };
 
